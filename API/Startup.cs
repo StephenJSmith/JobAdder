6c1af88ed1f387bf73bd02193aa9d0a0f1558f17
stephen-talentpay <stephen@talentpay.com>
2020-05-10 18:06:02 +1000

    Install and configure Swagger for API definitions

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -33,6 +34,14 @@ namespace API
       });
 
       services.AddApplicationServices();
+
+      services.AddSwaggerGen(options => {
+        options.SwaggerDoc("v1",
+          new OpenApiInfo {
+            Title = "JobAdder Coding Challenge API",
+            Version = "v1"
+          });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +57,13 @@ namespace API
       app.UseRouting();
 
       app.UseAuthorization();
+
+      app.UseSwagger();
+      app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint(
+          "/swagger/v1/swagger.json",
+          "JobAdder Coding Challenge API v1");
+      });
 
       app.UseEndpoints(endpoints =>
       {
